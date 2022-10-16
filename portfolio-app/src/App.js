e516ae3e1973513863ba7d16a0aaad201a72b6dc
sonjailic <sonjailic94@hotmail.com>
2022-10-15 22:11:40 -0400

    conditionally render the contat form

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Nav from './components/Nav'
 import About from './components/About'
 import Gallery from "./components/Gallery";
+import ContactForm from "./components/Contact"
 
 function App() {
   const [categories] = useState([
@@ -18,16 +19,26 @@ function App() {
   ]);
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
     <div>
       <Nav
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
       </main>
 
     </div>
